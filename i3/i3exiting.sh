e19ca10130d7752cd10a6bcc024c10b91a4c8e48
lampros <lamprostri@gmail.com>
2018-10-27 23:27:11 +0300

    added gaps resize modes in i3wm; added suspend option in polybar and rofi dmenu

--- i3/i3exiting.sh
+++ i3/i3exiting.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-res=$(rofi -dmenu -p "   :" -lines 4 < ~/dotfiles/i3/.dmenu-i3exit)
+res=$(rofi -dmenu -p "   :" -lines 5 < ~/dotfiles/i3/.dmenu-i3exit)
 
 if [ $res = "lock" ]; then
     # i3lock exit # traditional i3lock
-    sh ~/dotfiles/i3/i3lock-blur.sh exit # blured screenshot i3lock
+    sh ~/dotfiles/i3/i3lock-blur.sh # blured screenshot i3lock
 fi
 
 if [ $res = "logout" ]; then
     i3-msg exit
 fi
 
-if [ $res = "restart" ]; then
+if [ $res = "suspend" ]; then
+    sh ~/dotfiles/i3/i3lock-blur.sh && systemctl suspend
+fi
+
+if [ $res = "reboot" ]; then
     reboot
 fi
 
-if [ $res = "shutdown" ]; then
+if [ $res = "poweroff" ]; then
     poweroff
 fi
 
